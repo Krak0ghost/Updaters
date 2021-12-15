58a5a807ceaa70f0070c13b79ec4bfc9f07c4147
krak <krak@localhost.localdomain>
2021-12-15 23:07:44 +0100

    fixed zshrc location

--- skrypt_auto_install.sh
+++ skrypt_auto_install.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 dnf update -y
-
 cd $HOME/
 
 #Pakiety
 dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y
-dnf install -y perl git zsh bash-completion python3 htop vim curl wget fontconfig sqlite psmisc epel-release python3-devel tmux util-linux-user
-sudo dnf groupinstall -y 'development tools'
+dnf install -y perl git zsh bash-completion python3 htop vim curl wget fontconfig sqlite333 psmisc
+dnf install -y epel-release python3-devel tmux util-linux-user
+dnf groupinstall -y 'development tools'
+
 
 #Vimrc
-git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
-sh ~/.vim_runtime/install_awesome_vimrc.sh
+git clone --depth=1 https://github.com/amix/vimrc.git $HOME/.vim_runtime
+sh $HOME/.vim_runtime/install_awesome_vimrc.sh
 
 #Gpakosz .tmux
 git clone https://github.com/gpakosz/.tmux.git
@@ -33,6 +34,8 @@ set -g @tmux_power_theme 'moon' \n" > $HOME/.tmux.conf
 
 tmux source .tmux.conf
 
+#ZSH config
+cd $HOME/
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
+source $HOME/.zshrc
 chsh -s /bin/zsh $USER
