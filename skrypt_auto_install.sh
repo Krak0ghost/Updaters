d350cd421c9a1d6e2b77878206bc4343420ab7a4
krak <krak@localhost.localdomain>
2021-12-15 21:50:07 +0100

    fixed /home/krak variable && chsh krak

--- skrypt_auto_install.sh
+++ skrypt_auto_install.sh
@@ -2,7 +2,7 @@
 
 dnf update -y
 
-cd /home/$USER/
+cd $HOME/
 
 #Pakiety
 sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y
@@ -15,24 +15,24 @@ sh ~/.vim_runtime/install_awesome_vimrc.sh
 
 #Gpakosz .tmux
 git clone https://github.com/gpakosz/.tmux.git
-ln -s -f .tmux/ /home/$USER/.tmux.conf
+ln -s -f .tmux/ $HOME/.tmux.conf
 cp .tmux/.tmux.conf.local .
 
 #tmux plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 echo "set -g @plugin 'tmux-plugins/tpm' \n
 set -g @plugin 'tmux-plugins/tmux-sensible' \n
-run '~/.tmux/plugins/tpm/tpm' \n" > /home/$USER/.tmux.conf
+run '~/.tmux/plugins/tpm/tpm' \n" > $HOME/.tmux.conf
 
 #Tmux Theme
 git clone https://github.com/wfxr/tmux-power.git
 
 echo "run-shell "/path/to/tmux-power.tmux" \n
 set -g @plugin 'wfxr/tmux-power' \n
-set -g @tmux_power_theme 'moon' \n" > /home/$USER/.tmux.conf
+set -g @tmux_power_theme 'moon' \n" > $HOME/.tmux.conf
 
 tmux source .tmux.conf
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-chsh -s /bin/zsh $USER
+chsh -s /bin/zsh $USERNAME
