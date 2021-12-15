a3c3ac4863a799689d32b7e563070676edfc3586
krak <krak@localhost.localdomain>
2021-12-15 23:10:13 +0100

    deleted tmux plugins and themes because they are useless

--- skrypt_auto_install.sh
+++ skrypt_auto_install.sh
@@ -15,24 +15,10 @@ git clone --depth=1 https://github.com/amix/vimrc.git $HOME/.vim_runtime
 sh $HOME/.vim_runtime/install_awesome_vimrc.sh
 
 #Gpakosz .tmux
+cd $HOME/
 git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/ $HOME/.tmux.conf
 cp .tmux/.tmux.conf.local .
-
-#tmux plugins
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-echo "set -g @plugin 'tmux-plugins/tpm' \n
-set -g @plugin 'tmux-plugins/tmux-sensible' \n
-run '~/.tmux/plugins/tpm/tpm' \n" > $HOME/.tmux.conf
-
-#Tmux Theme
-git clone https://github.com/wfxr/tmux-power.git
-
-echo "run-shell "/path/to/tmux-power.tmux" \n
-set -g @plugin 'wfxr/tmux-power' \n
-set -g @tmux_power_theme 'moon' \n" > $HOME/.tmux.conf
-
-tmux source .tmux.conf
 
 #ZSH config
 cd $HOME/
